a0d27632b78b0f12c7bc422c5f9a486daa190ad0
kwizerao <o.kwizera@alustudent.com>
2022-10-28 06:11:39 -0700

    hard things

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 import hidden_4
-def main():
-    for name in dir(hidden_4):
-        if name.startswith('__'):
-            continue
-        print(name)
 if __name__ == '__main__':
-    main()
+    names = dir(hidden_4)
+    names.sort()
+    for name in names:
+        if name[:2] != "__":
+            print(name)
