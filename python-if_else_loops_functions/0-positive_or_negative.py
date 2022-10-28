af4bbf3cc92d085cef1118c666ea461bf11ea471
kwizerao <o.kwizera@alustudent.com>
2022-10-28 00:51:44 -0700

    hard things

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,36 +1,17 @@
 #!/usr/bin/python3
+
 import random
-
-"""this program prints the polority of a random number
-
-@arg is the nuber to be passed into the function"""
-
-
 
 number = random.randint(-10, 10)
 
-
-
-
-
-def pnive(arg):
+if number > 0:
     
-
+        print(f"{number} is positive")
+        
+elif number == 0:
     
-    if arg > 0:
+        print(f"{number} is zero")
         
-        print(f"{arg} is positive")
-        
-    elif arg < 0:
-        
-        print(f"{arg} is negative")
-        
-    else:
-        
-        print(f"{arg} is zero")
-        
-
-        
-
-        
-pnive(number)
+else:
+    
+        print(f"{number} is negative")
