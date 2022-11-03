0c87d2589c5974c90714471ef35d59423a7f15d6
kwizerao <o.kwizera@alustudent.com>
2022-11-03 03:08:02 -0700

    home

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,37 +1,19 @@
 #!/usr/bin/python3
-
 # Replace an element of a list at a specific position
-
 # @idx is the index to be replaced
-
 # @element is the new element to replace the element at index
 
-
-
-def replace_in_list(my_list, idx, element):
-    
+def replace_in_list(my_list, idx, element):    
     new_list = []
-    
     if idx < 0 or idx > (len(my_list) - 1):
-        
         return my_list
-    
     else:
-        
         my_list[idx] = element
-        
         new_list = my_list
-        
         return new_list
     
 
-    
-
-    
 # a = [1, 2, 3]
-
 # replace_in_list(a, 1, 4)
-
-
 
 # print(replace_in_list(a, 1, 4))
