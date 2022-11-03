78a72da76e20ec0e562ef8237866a61345a01c33
kwizerao <o.kwizera@alustudent.com>
2022-11-03 05:32:43 -0700

    home

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 
-def element_at(my_list, idx):
+def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return (None)
-    length = len(my_list)    
-    if idx > length - 1:
-        return (None)    
-    return(my_list[idx])
+        return my_list
+    if idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list 
