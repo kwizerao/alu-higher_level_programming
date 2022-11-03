540d331a604a2085bd1f106b9bd21bd997e6c9b9
kwizerao <o.kwizera@alustudent.com>
2022-11-03 05:23:20 -0700

    home

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,7 +4,9 @@
 def element_at(my_list, idx):
     if idx < 0:
         return (None)
-    length = len(my_list)    
+    length = len(my_list)
+    
     if idx > length - 1:
-        return (None)
+        return (None)    
+
     return(my_list[idx])
