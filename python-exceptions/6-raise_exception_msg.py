d9340fa7bd34e2de853da563e19db22e4a413414
kwizerao <o.kwizera@alustudent.com>
2022-11-09 00:52:25 -0800

    home

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
 
-def safe_print_list(my_list=[], x=0):
+def raise_exception_msg(message=""):
     try:
-        for i in range(x):
-            print("{}".format(my_list[i]), end="")
-        print()
-        return x
-    except IndexError:
-        print()
-        return i
+        raise NameError(message)
+    except NameError:
+        print(message)
