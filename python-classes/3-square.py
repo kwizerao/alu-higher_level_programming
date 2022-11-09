0472feab65e5cb8ec4b7cc3d0c8b3bfad8941ddc
kwizerao <o.kwizera@alustudent.com>
2022-11-09 02:15:57 -0800

    home

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,8 +3,7 @@
 
 
 class Square:
-    """Represent a square."""
-    
+    """Represent a square."""    
     def __init__(self, size=0):
         """Initialize a new square.
         Args:
@@ -15,7 +14,6 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size        
-
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
