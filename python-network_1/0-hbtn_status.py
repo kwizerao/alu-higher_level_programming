58531d04f4e1c436660e3ee5e2b4f5d7f4854a7f
kwizerao <o.kwizera@alustudent.com>
2022-11-23 09:32:22 +0000

    home

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-'''script that fetches https://alu-intranet.hbtn.io/status'''
+"""
+Module 0-hbtn_status.py
+"""
+
 import urllib.request
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+        html = response.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
