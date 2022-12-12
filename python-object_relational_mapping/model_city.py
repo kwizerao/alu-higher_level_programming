538c6aee9363936943da0e95fa8fcff37d3395ef
Jeff Trojan <j.dauda@alustudent.com>
2022-12-12 16:27:38 +0200

    Last task fix

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Defining a database class with sql-alchemy'''
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+Base = declarative_base()
+
+
+class City(Base):
+    '''A class defination of a table in the sql'''
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
